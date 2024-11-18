91f60eaf705d20f30e09a2661469b0da507be1d8
diomsg <social.network@s-email.de>
2024-11-18 11:57:08 +0100

    Release 2.0.0-1-alpha

--- locals/localization.enUS.lua
+++ locals/localization.enUS.lua
@@ -1,4 +1,4 @@
-local _, SRT = ...
+local addonName, SRT = ...
 
 SRT.localization = setmetatable({},{__index=function(self,key)
         geterrorhandler()("Skyriding Race Timer: Missing entry for '" .. tostring(key) .. "'")
@@ -13,12 +13,21 @@ L["silver-time"] = "|T616375:0|t Silver Time: %s sec"
 L["bronze-time"] = "|T616372:0|t Bronze Time"
 L["no-time"] = "no medal time available"
 
+L["race-normal"] = "Normal"
+L["race-advanced"] = "Advanced"
+L["race-reverse"] = "Reverse"
+L["race-challenge"] = "Challenge"
+L["race-challenge-reverse"] = "Reverse Challenge"
+L["race-storm-gryphon"] = "Storm Gryphon"
+
 L["personal-best-time"] = "Personal Best Time: %s sec"
 L["personal-best-time-no-race"] = "no race completed so far"
 L["personal-best-time-not-available"] = "personal best time not available"
 L["personal-best-time-faild"] = "new personal best time failed"
 
-L["race-display"] = "Race Display"
+L["display"] = "Race Display"
+L["display.name"] = "Enable 'Race Display'"
+L["display.tooltip"] = "Activates or deactivates the 'Race Display' during the skyriding race."
 L["display-mode.name"] = "Display Mode"
 L["display-mode.tooltip"] = "Defines whether the time should be displayed as a timer or countdown during the race."
 L["display-mode.value.0"] = "Timer"
@@ -30,11 +39,13 @@ L["display-horizontal-shift.name"] = "Horizontal Shift of the Display"
 L["display-horizontal-shift.tooltip"] = "Defines the relative horizontal position of the race display to the centre of the screen."
 L["display-vertical-shift.name"] = "Vertical Shift of the Display"
 L["display-vertical-shift.tooltip"] = "Defines the relative vertical position of the race display to the centre of the screen."
-L["fadeout-delay.name"] = "Fade-out Delay"
-L["fadeout-delay.tooltip"] = "Defines the time after a race until the display fades out."
+L["display-fadeout-delay.name"] = "Fade-out Delay"
+L["display-fadeout-delay.tooltip"] = "Defines the time after a race until the display fades out."
+
+L["overview"] = "Race Time Overview"
+L["overview.name"] = "Enable 'Race Time Overview'"
+L["overview.tooltip"] = "Activates or deactivates the 'Race Time Overview' next to the quest window."
 
 L["other-options"] = "Other Options"
-L["debug.name"] = "Debug Mode"
-L["debug.tooltip"] = "Activate or deactivate the debug mode."
-
-L["reload.popup"] = "Do you want to reload the UI to apply the change?"
+L["debug.name"] = "Enable 'Debug Mode'"
+L["debug.tooltip"] = "Activates or deactivates the 'Debug Mode'."
